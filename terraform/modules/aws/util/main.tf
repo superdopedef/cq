c3fd8c12111762aab49076017d560e6c4d656398
superdopedef <53094503+superdopedef@users.noreply.github.com>
2024-02-02 13:16:21 -0800

    Update main.tf to remove broken Centos deps

--- terraform/modules/aws/util/main.tf
+++ terraform/modules/aws/util/main.tf
@@ -5,27 +5,6 @@ resource "random_pet" "random_pet" {
 
 resource "random_uuid" "random_uuid" {}
 
-data "aws_ami" "centos" {
-  most_recent = true
-  owners      = ["aws-marketplace"]
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-  filter {
-    name   = "image-type"
-    values = ["machine"]
-  }
-  filter {
-    name   = "name"
-    values = [var.centos_search]
-  }
-}
-
 data "aws_ami" "debian" {
   most_recent = true
   owners = ["136693071363"]
