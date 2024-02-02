200768588c9dac0f41a256ea1b0c0c232c0f79b2
superdopedef <53094503+superdopedef@users.noreply.github.com>
2024-02-02 13:16:52 -0800

    Update outputs.tf for Centos

--- terraform/modules/aws/util/outputs.tf
+++ terraform/modules/aws/util/outputs.tf
@@ -6,10 +6,6 @@ output "random_uuid" {
   value = random_uuid.random_uuid.id
 }
 
-output "latest_centos_ami" {
-  value = data.aws_ami.centos.id
-}
-
 output "latest_debian_ami" {
   value = data.aws_ami.debian.id
 }
